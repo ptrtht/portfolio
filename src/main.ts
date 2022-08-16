57da479f08bba70d7338e73d15ac61a86c70c56a
qty0 <peterszabototh@gmail.com>
2022-08-16 12:24:21 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.css"
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
